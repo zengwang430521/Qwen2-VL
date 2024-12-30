76189bbe49248d66fa48f49590dc7f61d0ef9d65
zengwang <zengwang@tetras.ai>
2024-12-30 19:19:15 +0800

    debug

--- test/test_video2.py
+++ test/test_video2.py
@@ -1,5 +1,5 @@
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
-from qwen_vl_utils import process_vision_info
+from qwen_vl_utils import process_vision_info, fetch_video
 import torch
 
 
@@ -16,48 +16,60 @@ processor = AutoProcessor.from_pretrained(model_ckpt)
 
 
 video_path = '/afs/zengwang/projects/task_define_service/data/video_event/push-up_2.mp4'
-time_spot = [2, 15, 21, 43, 51]
+time_spots = [2, 15, 21, 43, 54]
 query = 'Please narrate the video in real time.'
+fps = 5
 
+video_info = {
+    "type": "video",
+    "video": video_path,
+    "max_pixels": 256 * 28 * 28,
+    "fps": fps,
+}
+all_frames = fetch_video(video_info)
 
+video_clips = []
+idx_start = 0
+for t in time_spots:
+    idx_end = min(int(t * fps), all_frames.shape[0])
+    video_clips.append(all_frames[idx_start:idx_end])
+    idx_start = idx_end
 
-messages = [
-    {
-        "role": "user",
-        "content": [
-            {
-                "type": "video",
-                "video": video_path,
-                "max_pixels": 256 * 28 * 28,
-                "fps": 5.0,
-            },
-            {"type": "text", "text": "Describe this video."},
-        ],
-    }
-]
+# 初始messages
+messages = [{
+    "role": 'user',
+    'content': [
+        {"type": "text", "text": query},
+        video_info
+    ]
+}]
+image_inputs = None
+video_inputs = []
+for t, clip in zip(time_spots, video_clips):
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    video_inputs.append(clip)
 
-import pdb; pdb.set_trace()
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    inputs = inputs.to("cuda")
 
-# Preparation for inference
-text = processor.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True
-)
-image_inputs, video_inputs = process_vision_info(messages)
-inputs = processor(
-    text=[text],
-    images=image_inputs,
-    videos=video_inputs,
-    padding=True,
-    return_tensors="pt",
-)
-inputs = inputs.to("cuda")
+    # Inference
+    generated_ids = model.generate(**inputs, max_new_tokens=128)
+    generated_ids_trimmed = [
+        out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+    ]
+    output_text = processor.batch_decode(
+        generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )
+    print(f'Time {t} s:')
+    print(F'Assistant: {output_text}')
 
-# Inference
-generated_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-print(output_text)
+    messages.append({"role": 'assistant', 'content': output_text})
+    messages.append({"role": 'user', 'content': [video_info]})
